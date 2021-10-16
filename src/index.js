fad5621a37b47bf668046566e8b1078b1a66d612
João Bispo <joaobispo2077@gmail.com>
2021-10-16 10:09:15 -0300

    fix: use method findIndex instead 'findindex'

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
